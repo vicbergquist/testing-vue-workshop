2c44dc4162ba36d626afb4b04ad821e119982d98
eddyerburgh <edward.yerburgh@gmail.com>
2018-09-19 17:38:32 +0100

    Add cypress

--- projects/hackernews-tested/cypress/integration/journeys.spec.js
+++ projects/hackernews-tested/cypress/integration/journeys.spec.js
@@ -1,6 +1,22 @@
+/* global cy */
+
 describe('Hacker News', () => {
-  it('renders list of items with comments', () => {
-    cy.visit('https://example.cypress.io')
-    cypress.get
+  it('goes to item page when clicking comments link', () => {
+    cy.visit('/top')
+    cy.get('.comments-link a')
+      .first()
+      .click()
+    cy.url().should('contain', `/item`)
+    cy.get('.item-view-comments-header').contains(/[0-9]* comments/)
+  })
+
+  it('changes list items when clicking between list types', () => {
+    cy.visit('/top')
+    cy.contains('New').click()
+    cy.get('.item-list')
+    cy.contains('Show').click()
+    cy.get('.item-list')
+    cy.contains('Ask').click()
+    cy.get('.item-list')
   })
 })
