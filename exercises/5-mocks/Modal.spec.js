69802b68d21732efe161cef940196d537b2c5157
vicbergquist <victoria.bergquist@gmail.com>
2018-09-20 15:04:07 +0100

    exercise 5

--- exercises/5-mocks/Modal.spec.js
+++ exercises/5-mocks/Modal.spec.js
@@ -2,4 +2,18 @@ import { shallowMount } from '@vue/test-utils'
 import Modal from './Modal.vue'
 
 // Use jest.fn to create a mock function
-test('calls onClose when close button is clicked')
+test('calls onClose when close button is clicked', () => {
+  const onClose = jest.fn()
+  // getting the props from our Modal component
+  const wrapper = shallowMount(Modal, {
+    propsData: {
+      onClose
+    }
+  })
+  // find the element with the right aria label
+  // then trigger the click
+  wrapper.find('[aria-label="close"]').trigger('click')
+
+  // test to see if the onClose function was called after the click event was triggered
+  expect(onClose).toHaveBeenCalled()
+})
--- exercises/5-mocks/SubmitForm.spec.js
+++ exercises/5-mocks/SubmitForm.spec.js
@@ -1,4 +1,19 @@
 import { shallowMount } from '@vue/test-utils'
 import SubmitForm from './SubmitForm.vue'
 
-test('calls onSubmit with value of input when form is submitted')
+test('calls onSubmit with value of input when form is submitted', () => {
+  // Setting up the submit fn as a mock fn
+  const onSubmit = jest.fn()
+  // Getting our element
+  const wrapper = shallowMount(SubmitForm, {
+    propsData: {
+      onSubmit
+    }
+  })
+  // Find the input and set a value to test against
+  wrapper.find('[type="text"]').setValue('Hello World')
+  // find the input and trigger a submit event
+  wrapper.find('[type="text"]').trigger('submit')
+  // Test that the fn was called with our speciified text
+  expect(onSubmit).toHaveBeenCalledWith('Hello World')
+})
