8c7e08795687de31372c446da403dd9fe3f7ab0c
vicbergquist <victoria.bergquist@gmail.com>
2018-09-20 15:04:15 +0100

    exercise 6

--- exercises/6-async-code/ItemView.spec.js
+++ exercises/6-async-code/ItemView.spec.js
@@ -1,5 +1,21 @@
+import { shallowMount } from '@vue/test-utils'
+import fetchItems from './api'
+import axios from 'axios'
 import ItemView from './ItemView'
+import Item from './Item.vue'
+import flushPromises from 'flush-promises'
 
-test('renders items resolved from fetchItems')
+jest.mock('./api')
+
+test('renders items resolved from fetchItems', async () => {
+  expect.assertions(1)
+  const cats = [{},{}]
+  fetchItems.mockResolvedValue(cats)
+  const wrapper = shallowMount(ItemView)
+
+  await flushPromises()
+
+  expect(wrapper.findAll(Item)).toHaveLength(2)
+})
 
 test('renders error if fetchItems rejects')
--- exercises/6-async-code/api.spec.js
+++ exercises/6-async-code/api.spec.js
@@ -1,3 +1,27 @@
-test('resolves with data.items if /items returns 200')
+import { fetchItems } from './api'
+import axios from 'axios'
 
-test('rejects with error if /items returns 500')
+// Let jest know we are mocking axios
+jest.mock('axios')
+
+test('resolves with data.items if /items returns 200', async () => {
+  // Let the test know how many assertions are being called here, to make sure that assertions in a callback got called
+  expect.assertions(1)
+  // What want to check the api response against
+  const cats = [{},{},{}]
+  // Make the mock request, mockResolvedValue 200
+  axios.get.mockResolvedValue(cats)
+  // store the response
+  const data = await fetchItems()
+  // assertion
+  expect(data).toBe(cats)
+})
+
+test('rejects with error if /items returns 500', async () => {
+  // assertions we are expecting
+  expect.assertions(1)
+  // creating the mock value for this call
+  axios.get.mockRejectedValue(new Error('500'))
+  // expect that the fetchItems call get an error
+  expect(fetchItems()).rejects.toBeInstanceOf(Error)
+})
