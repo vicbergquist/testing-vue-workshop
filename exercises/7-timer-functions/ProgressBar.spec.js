a6f0c343d2ea9696b9c533357e594a6d25ca8bd7
vicbergquist <victoria.bergquist@gmail.com>
2018-09-20 15:04:22 +0100

    exercise 7

--- exercises/7-timer-functions/ProgressBar.spec.js
+++ exercises/7-timer-functions/ProgressBar.spec.js
@@ -1,1 +1,17 @@
-test('increments width by 1% every 100ms')
+import ProgressBar from './ProgressBar.vue'
+import {shallowMount} from '@vue/test-utils'
+
+test('increments width by 1% every 100ms', () => {
+  // let the test know we are faking timers
+  jest.useFakeTimers()
+  const wrapper = shallowMount(ProgressBar)
+
+  // Advance the timer by 400, then check that the output is 4%
+  jest.advanceTimersByTime(400)
+  expect(wrapper.element.style.width).toBe('4%')
+
+  // advance the timer to 700, then check that the output is 400+700
+  jest.advanceTimersByTime(700)
+  expect(wrapper.element.style.width).toBe('11%')
+
+})
