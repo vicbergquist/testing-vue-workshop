e4363093e2a2bc5cee1f5755c528de3b5f7946c7
vicbergquist <victoria.bergquist@gmail.com>
2018-09-20 15:03:28 +0100

    exercise 3

--- exercises/3-vue-events/App.spec.js
+++ exercises/3-vue-events/App.spec.js
@@ -1,1 +1,13 @@
-test('removes Modal when close-modal is emitted')
+import {shallowMount} from '@vue/test-utils'
+import App from './App.vue'
+import Modal from './Modal.vue'
+
+test('removes Modal when close-modal is emitted', () => {
+  // Get the component you're testing
+  const wrapper = shallowMount(App)
+  // Set up what you're looking for, close-modal string is emitted
+  wrapper.find(Modal).vm.$emit('close-modal')
+  // What the test is to expect, close-modal does not exist. Can also use find.exists().toBe(false)
+  expect(wrapper.findAll(Modal)).toHaveLength(0)
+
+})
--- exercises/3-vue-events/Modal.spec.js
+++ exercises/3-vue-events/Modal.spec.js
@@ -1,1 +1,18 @@
-test('emits on-close event when close button is clicked')
+import {shallowMount} from '@vue/test-utils'
+import Modal from './Modal.vue'
+
+test('emits on-close event when close button is clicked', () => {
+  // Our Modal component
+  const wrapper = shallowMount(Modal)
+
+  // Making sure it hasn't been emitted already. It's undefined because it hasnt been emitted.
+  expect(wrapper.emitted('close-modal')).toBeUndefined()
+
+  // Find the element that is to be triggered
+  wrapper.find('[aria-label="close"]').trigger('click')
+
+  // Check that close-modal was called. Length of 1 indicates it was emitted
+  // Only returns only the close-model event. Can also call without event name, and it will return all events.
+  expect(wrapper.emitted('close-modal')).toHaveLength(1)
+
+})
