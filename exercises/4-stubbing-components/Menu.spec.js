dfb0ad9ab9e68045c5629ed3c3b8733ea9d077b4
vicbergquist <victoria.bergquist@gmail.com>
2018-09-20 15:03:51 +0100

    exercise 4

--- exercises/4-stubbing-components/Menu.spec.js
+++ exercises/4-stubbing-components/Menu.spec.js
@@ -1,3 +1,36 @@
-test('renders router-link to /login page when signedIn is false')
+import { RouterLinkStub, shallowMount } from '@vue/test-utils'
+import Menu from './Menu.vue'
 
-test('renders <a> that signs user out if signedIn is true')
+test('renders router-link to /login page when signedIn is false', () => {
+  // Make the stub from our component's routerlink
+  const wrapper = shallowMount(Menu, {
+    stubs: {
+      'router-link': RouterLinkStub
+    }
+  })
+  // Find the stub, and find the prop value and check that it is /login.
+  // Can't be on /login if you're signed in, so if /login is true, signedIn is false
+  expect(wrapper.find(RouterLinkStub).props('to')).toBe('/login')
+})
+
+test('renders <a> that signs user out if signedIn is true', () => {
+  // Creates a mock function
+  const signOut = jest.fn()
+  const wrapper = shallowMount(Menu, {
+    propsData: {
+      signedIn: true,
+      signOut
+    },
+    stubs: {
+      'router-link': RouterLinkStub
+    }
+  })
+  // Find the a tag and trigger a click event
+  wrapper.find('a').trigger('click')
+
+  // Find the router link stubs
+  expect(wrapper.findAll(RouterLinkStub)).toHaveLength(0)
+
+  // test if the signout mock function has been called
+  expect(signOut).toHaveBeenCalled()
+})
